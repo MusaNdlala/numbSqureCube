6f7725bee5cc5df72a0eabb39426027989ff7e70
Musa <Musa@DESKTOP-N08JB8A>
2021-09-10 17:36:22 +0200

    Add project files.

---
+++ numbSqureCube/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace numbSqureCube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            multiples();
+        }
+        static void multiples()
+        {
+            int iterator = 0;
+            Console.WriteLine("number   Square   cube");
+            while (iterator <= 10)
+            {
+                Console.Write(iterator + "      ");
+                Console.Write(iterator * iterator+ "       ");
+                Console.WriteLine(iterator * iterator * iterator);
+                iterator++;
+            }
+            /*for (int iterator = 10; iterator <= 0; iterator--)
+            {
+                Console.Write(iterator + "      ");
+                Console.Write(iterator * iterator + "       ");
+                Console.WriteLine(iterator * iterator * iterator);
+            }*/
+            Console.ReadLine();
+        }
+    }
+}
